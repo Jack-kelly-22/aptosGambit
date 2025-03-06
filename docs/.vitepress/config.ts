24142e1eaafaf79e929cc129a94eb00c115e07bd
jack-kelly-22 <jacklaxjk@gmail.com>
2025-03-06 10:51:46 -0700

    init pages

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   // ... existing config ...
   
+  
   // Add these lines:
   base: '/aptos-gambit/', // Use your repository name
   outDir: '.vitepress/dist',
