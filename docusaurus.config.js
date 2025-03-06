bd693a1ee37d49293c3d2f942af03def0fe88973
jack-kelly-22 <jacklaxjk@gmail.com>
2025-03-06 13:51:12 -0700

    config update

---
+++ docusaurus.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // ... other config
+  url: 'https://jack-kelly-22.github.io',
+  baseUrl: '/aptosGambit/',
+  organizationName: 'jack-kelly-22', // Usually your GitHub org/user name.
+  projectName: 'aptosGambit', // Usually your repo name.
+  trailingSlash: false,
+  // ... rest of your config
+}; 
